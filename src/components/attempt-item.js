2f4b5bb3cf4d8cdd65f0a3cb094f3e0c831dc999
Moisés Hilario <moises.rodrigues@codeminer42.com>
2018-07-31 17:29:48 -0300

    Change attempt component name

--- src/components/attempt-item.js
+++ src/components/attempt-item.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const Attempt = ({ attemptNumber, password, close, match }) => (
+export const AttemptItem = ({ attemptNumber, password, close, match }) => (
   <li className="attempt__item">
     <span className="attempt__item__number">{attemptNumber}</span>
     <h3 className="attempt__item__password">{password}</h3>
     <div className="attempt__item__report">
-      <p className="report__close">{ close }</p>
-      <p className="report__match">{ match }</p>
+      <p className="report__close">Close: { close }</p>
+      <p className="report__match">Match: { match }</p>
     </div>
   </li>
 )
 
-Attempt.propTypes = {
+AttemptItem.propTypes = {
   attemptNumber: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
   close: PropTypes.string.isRequired,
