b54ad7fd507b61f1bc566e8584d71baaf9eb3885
Moisés Hilario <moises.rodrigues@codeminer42.com>
2018-07-31 15:48:14 -0300

    Create attempt component

--- src/components/attempt.js
+++ src/components/attempt.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 
+export const Attempt = ({ attemptNumber, password, close, match }) => (
+  <li className="attempt__item">
+    <span className="attempt__item__number">{attemptNumber}</span>
+    <h3 className="attempt__item__password">{password}</h3>
+    <div className="attempt__item__report">
+      <p className="report__close">{ close }</p>
+      <p className="report__match">{ match }</p>
+    </div>
+  </li>
+)
+
+Attempt.propTypes = {
+  attemptNumber: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  close: PropTypes.string.isRequired,
+  match: PropTypes.string.isRequired
+}
